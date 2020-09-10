ee92f01da48547dd7087f5272638d8755a74cfc7
Matias Salicru <mattiassalicru@gmail.com>
2020-09-10 19:10:22 -0300

    Nosotros adapted for mobile

--- src/Routes/Componentes/Footer/Footer.js
+++ src/Routes/Componentes/Footer/Footer.js
@@ -8,47 +8,49 @@ import location from "../../../Assets/images/location.svg";
 function Footer() {
   return (
     <div className="footer-container">
+      <div className="links-container">
         <div className="item">
-          <img className='icon-social' src={insta} alt="*" />
           <a
             href="https://www.instagram.com/upo_bastidores/"
             target="_Blank"
             rel="noopener noreferrer"
           >
-            Upo Bastidores
+            <img className="icon-social" src={insta} alt="*" />
+            <p>Upo Bastidores</p>
           </a>
         </div>
         <div className="item">
-          <img className='icon-social' src={fb} alt="*" />
           <a
             href="https://www.facebook.com/upo_bastidores-101914031541600"
             target="_Blank"
             rel="noopener noreferrer"
           >
-            Upo Bastidores
+            <img className="icon-social" src={fb} alt="*" />
+            <p>Upo Bastidores</p>
           </a>
         </div>
         <div className="item">
-          <img className='icon-social' src={wsp} alt="*" />
           <a
-            href="https://wa.div/x16smx"
+            href="https://wa.link/x16smx"
             target="_Blank"
             rel="noopener noreferrer"
           >
-            3517174900
+            <img className="icon-social" src={wsp} alt="*" />
+            <p>3517174900</p>
           </a>
         </div>
         <div className="item">
-          <img className='icon-social' src={location} alt="*" />
           <a
             href="https://www.google.com/maps/place/C%C3%B3rdoba/data=!4m2!3m1!1s0x9432985f478f5b69:0xb0a24f9a5366b092?sa=X&ved=2ahUKEwic7M-6jKvrAhWkHLkGHQYqBgYQ8gEwAHoECAsQAQ"
             target="_Blank"
             rel="noopener noreferrer"
           >
-            Cordoba, Argentina
+            <img className="icon-social" src={location} alt="*" />
+            <p>Cordoba, Argentina</p>
           </a>
         </div>
       </div>
+    </div>
   );
 }
 
